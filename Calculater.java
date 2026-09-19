2737e3c92fda86ab3cecdc87465498a63a19a144
shaktiswaroopkushwaha61-lang <shaktiswaroopkushwaha61@gmail.com>
2026-09-19 15:08:55 +0530

    Update and rename calculater.java to Calculater.java

--- Calculater.java
+++ Calculater.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class calculater {
+public class Calculater {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("enter first number;");
